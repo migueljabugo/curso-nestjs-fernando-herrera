a9092e02fa9d51c081380a68590aa3f96d940127
ALTEDA\miguelangel.gonzalez <migueljabugo@gmail.com>
2026-02-05 16:11:50 +0100

    12. tipos de dato

--- Nest_Desarrollo_backend_escalable_con_Node/01-typescript-intro/src/bases/01-types.ts
+++ Nest_Desarrollo_backend_escalable_con_Node/01-typescript-intro/src/bases/01-types.ts
@@ -4,4 +4,16 @@ export const age = 32;
 export const isValid: boolean = true;
 
 
+export const templateString = `Esto es un string
+multilinea
+que puede tener
+" dobles
+' simples
+inyectar valores ${name}
+expresiones ${1+1}
+números ${age}
+booleanos ${isValid}
+`;
+
+console.log(templateString);
 
