1aae404a12a09298703a3142f83d6ae2b1c5503c
ALTEDA\miguelangel.gonzalez <migueljabugo@gmail.com>
2026-02-05 13:20:32 +0100

    5. Actualización de un objeto TODO en la lista estática

--- 01-foundation/src/todo/dto/update-todo.dto.ts
+++ 01-foundation/src/todo/dto/update-todo.dto.ts
@@ -1,4 +1,11 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateTodoDto } from './create-todo.dto';
+import { IsBoolean, isBoolean, IsOptional } from 'class-validator';
 
-export class UpdateTodoDto extends PartialType(CreateTodoDto) {}
+export class UpdateTodoDto extends PartialType(CreateTodoDto) {
+
+    @IsBoolean()
+    @IsOptional()
+    done?: boolean;
+
+}
