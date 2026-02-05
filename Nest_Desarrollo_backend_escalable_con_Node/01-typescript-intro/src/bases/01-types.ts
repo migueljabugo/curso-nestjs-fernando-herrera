adf167f022b9f7ab212808982e9c88165e2b4a3a
ALTEDA\miguelangel.gonzalez <migueljabugo@gmail.com>
2026-02-05 16:04:38 +0100

    11. introducción

---
+++ Nest_Desarrollo_backend_escalable_con_Node/01-typescript-intro/src/bases/01-types.ts
@@ -0,0 +1,7 @@
+
+export const name: string = 'Miguel Angel';
+export const age = 32;
+export const isValid: boolean = true;
+
+
+
--- Nest_Desarrollo_backend_escalable_con_Node/01-typescript-intro/src/main.ts
+++ Nest_Desarrollo_backend_escalable_con_Node/01-typescript-intro/src/main.ts
@@ -1,7 +1,8 @@
 import './style.css'
 import typescriptLogo from './typescript.svg'
 import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import { name, age } from './bases/01-types'
+
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
@@ -11,14 +12,8 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
     <a href="https://www.typescriptlang.org/" target="_blank">
       <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
     </a>
-    <h1>Vite + TypeScript!!!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
+    <h1>Hello ${name}! ${age}</h1>
   </div>
 `
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+
