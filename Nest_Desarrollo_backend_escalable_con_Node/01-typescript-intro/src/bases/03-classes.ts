cc6a461f23d1fd75ed57da9c307f70f2c5f2aabb
ALTEDA\miguelangel.gonzalez <migueljabugo@gmail.com>
2026-02-05 16:48:31 +0100

    16. Getters, métodos y THIS

--- Nest_Desarrollo_backend_escalable_con_Node/01-typescript-intro/src/bases/03-classes.ts
+++ Nest_Desarrollo_backend_escalable_con_Node/01-typescript-intro/src/bases/03-classes.ts
@@ -2,11 +2,34 @@
 export class Pokemon {
     public id: number;
     public name: string;
+    //public imageUrl: string;
 
-    constructor( id: number, name: string ) {
+    get imageUrl(): string {
+        return `https://pokemon.com/${this.id}.jpg`;
+    }
+
+    constructor( 
+        id: number, 
+        name: string/*, 
+        imageUrl: string*/) {
+
         this.id = id;
         this.name = name;
+        //this.imageUrl=imageUrl
+    }
+
+    /*public*/scream(){
+        console.log(`${this.name.toUpperCase()}!!!`);
+    }
+
+    /*private*/speak(){
+        console.log(`${this.name}, ${this.name}`);
     }
 }
 
 export const charmander = new Pokemon(4, 'Charmander');
+
+charmander.scream();
+charmander.speak();
+
+console.log(charmander.imageUrl);
