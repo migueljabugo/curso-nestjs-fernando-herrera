11748916aa2ed95649229954611d050b02400663
ALTEDA\miguelangel.gonzalez <migueljabugo@gmail.com>
2026-02-15 14:11:27 +0100

    35. Devolver el coche por el ID desde el parametro

--- Nest_Desarrollo_backend_escalable_con_Node/02-car-dealership/src/cars/cars.controller.ts
+++ Nest_Desarrollo_backend_escalable_con_Node/02-car-dealership/src/cars/cars.controller.ts
@@ -1,9 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('cars')
 export class CarsController {
+  private cars = ['Toyota', 'Honda', 'Suzuki'];
+
   @Get()
   getAllCars() {
-    return ['toyota', 'honda', 'suzuki'];
+    return this.cars;
+  }
+
+  @Get(':id')
+  getCarById(@Param('id') id: number) {
+    return this.cars[id];
   }
 }
