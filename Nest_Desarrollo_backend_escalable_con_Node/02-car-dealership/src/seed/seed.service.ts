fd3f71937cdc1937f29bf5ddc0487921a5ac154e
Miguel Angel González <migueljabugo@gmail.com>
2026-02-18 00:16:55 +0100

    63. Damos funcionalidad al endpoint PopulateDB. Importante, la importación de los modulos y la exportación en estos de sus servicios

--- Nest_Desarrollo_backend_escalable_con_Node/02-car-dealership/src/seed/seed.service.ts
+++ Nest_Desarrollo_backend_escalable_con_Node/02-car-dealership/src/seed/seed.service.ts
@@ -1,11 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { CarsService } from '../cars/cars.service';
+import { BrandsService } from 'src/brands/brands.service';
+import { CAR_SEED } from './data/cars.seed';
+import { BRAND_SEED } from './data/brands.seed';
 
 @Injectable()
 export class SeedService {
 
+  constructor(
+    private readonly carsService: CarsService,
+    private readonly brandsService: BrandsService,
+  ) { }
+
+
 
   populateDB() {
-
+    this.carsService.fillCarsWithSeedData(CAR_SEED);
+    this.brandsService.fillBrandsWithSeedData(BRAND_SEED);
+  
 
     return "Seed executed sucessfully!!";
   }
