3ebf5432297e03a92b385ad658e37cf7ccc4c3aa
Miguel Angel González <migueljabugo@gmail.com>
2026-02-17 22:07:22 +0100

    49. Validación de los DTOs a nivel de aplicación

--- Nest_Desarrollo_backend_escalable_con_Node/02-car-dealership/src/cars/cars.controller.ts
+++ Nest_Desarrollo_backend_escalable_con_Node/02-car-dealership/src/cars/cars.controller.ts
@@ -7,14 +7,12 @@ import {
   ParseUUIDPipe,
   Patch,
   Post,
-  UsePipes,
-  ValidationPipe,
 } from '@nestjs/common';
 import { CarsService } from './cars.service';
 import { CreateCarDto } from './dto/create-car.dto';
 
 @Controller('cars')
-@UsePipes(ValidationPipe)
+//@UsePipes(ValidationPipe)
 export class CarsController {
   constructor(private readonly carsService: CarsService) {}
 
--- Nest_Desarrollo_backend_escalable_con_Node/02-car-dealership/src/main.ts
+++ Nest_Desarrollo_backend_escalable_con_Node/02-car-dealership/src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, //Elimina la información que no esté expecificada en el DTO.
+      forbidNonWhitelisted: true, //Devuelve un error cuando se recibe información adicional a la que define el DTO
+    }),
+  );
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
