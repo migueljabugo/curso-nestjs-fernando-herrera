3cf7d5baa67f2c173b54f6d4b12f85989527e7c4
Miguel Angel González <migueljabugo@gmail.com>
2026-02-17 23:50:57 +0100

    60. Crud completo de Marcas

--- Nest_Desarrollo_backend_escalable_con_Node/02-car-dealership/src/brands/brands.service.ts
+++ Nest_Desarrollo_backend_escalable_con_Node/02-car-dealership/src/brands/brands.service.ts
@@ -1,26 +1,60 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import {v4 as uuid } from 'uuid';
 import { CreateBrandDto } from './dto/create-brand.dto';
 import { UpdateBrandDto } from './dto/update-brand.dto';
+import { Brand } from './entities/brand.entity';
 
 @Injectable()
 export class BrandsService {
+  private Brands: Brand[] = [
+    {
+      id: uuid(),
+      name: 'Toyota',
+      createAt: new Date().getTime(),
+      updateAt: new Date().getTime(),
+    }
+  ];
+
   create(createBrandDto: CreateBrandDto) {
-    return 'This action adds a new brand';
+    const brand: Brand = {
+      id: uuid(),
+      name: createBrandDto.name.toLowerCase(),
+      createAt: new Date().getTime(),
+    };
+    
+    this.Brands.push(brand);
+
+    return brand;
   }
 
   findAll() {
-    return `This action returns all brands`;
+    return this.Brands;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} brand`;
+  findOne(id: string) {
+    const brand = this.Brands.find(brand => brand.id === id);
+    if (!brand) throw new NotFoundException(`Brand with id "${id}" not found`);
+    return brand;
   }
 
-  update(id: number, updateBrandDto: UpdateBrandDto) {
-    return `This action updates a #${id} brand`;
+  update(id: string, updateBrandDto: UpdateBrandDto) {
+    let brandDB = this.findOne(id);
+    this.Brands = this.Brands.map(brand => {
+      if (brand.id === id) {
+        brandDB.updateAt = new Date().getTime();
+        brandDB = { 
+          ...brandDB, 
+          ...updateBrandDto,
+        };
+        return brandDB;
+      }
+      return brand;
+    });
+
+    return brandDB;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} brand`;
+  remove(id: string) {
+    this.Brands = this.Brands.filter(brand => brand.id !== id);
   }
 }
--- Nest_Desarrollo_backend_escalable_con_Node/02-car-dealership/src/brands/entities/brand.entity.ts
+++ Nest_Desarrollo_backend_escalable_con_Node/02-car-dealership/src/brands/entities/brand.entity.ts
@@ -1,1 +1,9 @@
-export class Brand {}
+export class Brand {
+
+    id: string;
+    name: string;
+    
+    createAt: number;
+    updateAt?: number;
+
+}
