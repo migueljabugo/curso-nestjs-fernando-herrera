2a22e4c8016067f832a2c4414bc3e7b449bf72e8
Miguel Angel González <migueljabugo@gmail.com>
2026-02-17 22:11:27 +0100

    50. Tarea de creación de un nuevo coche mediante el post

--- Nest_Desarrollo_backend_escalable_con_Node/02-car-dealership/src/cars/cars.controller.ts
+++ Nest_Desarrollo_backend_escalable_con_Node/02-car-dealership/src/cars/cars.controller.ts
@@ -29,7 +29,7 @@ export class CarsController {
 
   @Post()
   createCar(@Body() createCarDto: CreateCarDto): any {
-    return createCarDto;
+    return this.carsService.create(createCarDto);
   }
 
   @Patch(':id')
